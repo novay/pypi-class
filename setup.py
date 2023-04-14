a16136210a187480ee9cc8f6bf32e1b6e3d8cbf2
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2023-04-15 06:02:55 +0800

    Change

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
+from codecs import open
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
-    name='pypi_class',
-    version='0.1.0',
-    description='Pypi Class - Contoh library menggunakan class.',
-    author='Novianto Rahmiad',
-    author_email='novay@btekno.id',
-    packages=find_packages(),
+    name="pypi_class",
+    version="0.0.1",
+    description="Contoh Library Python dengan Class.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/novay/pypi-class",
+    author="Novianto Rahmadi",
+    author_email="novay@btekno.id",
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
     ],
+    packages=["pypi_class"]
 )
